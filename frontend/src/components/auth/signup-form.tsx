15c363573ebdb46d44b71eaad8540e9b862f18c8
Ujjwal Tyagi <145588431+Ujjwaltyagi295@users.noreply.github.com>
2025-09-30 17:23:41 +0530

    Update signup-form.tsx

--- frontend/src/components/auth/signup-form.tsx
+++ frontend/src/components/auth/signup-form.tsx
@@ -140,7 +140,8 @@ export function SignupForm({
                   Login
                 </a>
               </div>
-              <div className="relative text-center text-sm after:absolute after:inset-0 after:top-1/2 after:z-0 after:flex after:items-center after:border-t after:border-border">
+              {/*
+                <div className="relative text-center text-sm after:absolute after:inset-0 after:top-1/2 after:z-0 after:flex after:items-center after:border-t after:border-border">
                 <span className="relative z-10 bg-background px-2 text-muted-foreground">
                   Or continue with
                 </span>
@@ -156,6 +157,8 @@ export function SignupForm({
                   Signup with Google
                 </Button>
               </div>
+              
+              */}
             </div>
           </form>
         </CardContent>
